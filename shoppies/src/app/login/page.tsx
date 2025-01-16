8509a3a0f6368ec293146574361a0ef98db249d9
Pranav <e1155906@u.nus.edu>
2025-01-16 11:18:51 +0800

    login page v1

---
+++ shoppies/src/app/login/page.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+// src/components/LoginPage.tsx
+import React, { useState } from 'react';
+import './LoginPage.css';
+
+
+const LoginPage: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1>Sign in for faster checkout.</h1>
+      <h2>Sign in to Shoppies</h2>
+
+      <div className="login-row">
+        <input
+          className="login-input"
+          type="text"
+          placeholder="Email or Phone Number"
+        />
+      <div className="password-row">
+        <input
+          className="login-input"
+          placeholder="Password"
+          type="password"
+    />
+    <button className="arrow-btn">→</button>
+  </div>
+      </div>
+
+      <div className="remember-me">
+        <input type="checkbox" id="remember" />
+        <label htmlFor="remember">Remember me</label>
+      </div>
+
+      <p><a href="#forgot">Forgot password?</a></p>
+      <p>Don’t have an Account? <a href="#create">Create yours now.</a></p>
+      <p>Need some help? <a href="#chat">Chat now</a></p>
+    </div>
+  );
+};
+
+export default LoginPage;
